975ac5889d34d36ff5d20ca55d699c96588cb34e
Seroga <seroga801@gmail.com>
2021-07-22 10:27:00 +0300

    add model Product-Images

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,37 @@ from django.contrib import admin
 
 from store.models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Product admin implementation"""
+
+    fields = [
+        "brand",
+        "image",
+        "slug",
+        "product",
+        "description",
+        "price",
+        "quantity",
+    ]
+
+    prepopulated_fields = {"slug": ["product"]}
+
+    list_display = [
+        "product",
+        "brand",
+        "price",
+        "quantity",
+    ]
+
+    list_display_links = [
+        "product",
+    ]
+
+    list_editable = [
+        "price",
+        "quantity",
+    ]
+
+    list_filter = ["brand"]
